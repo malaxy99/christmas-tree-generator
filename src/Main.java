2c98bd20b418a34a78e60edcdb8d331c08fffebc
Abel <weldegebrielabel@gmail.com>
2023-07-29 18:28:03 -0400

    Updating Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
+
+        // calling final christmas tree making method
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a height for tree (bigger than 3): ");
         int ht = input.nextInt();
@@ -12,6 +12,7 @@ public class Main {
     }
 
     public static void printStars(int number) {
+        //method for printing n amount of asterisks
         for(int i = 1; i <= number; ++i) {
             System.out.print("*");
         }
@@ -19,23 +20,15 @@ public class Main {
     }
 
     public static void printSpaces(int number) {
+        //method for printing n amount of spaces
         for(int i = 1; i <= number; ++i) {
             System.out.print(" ");
         }
 
     }
 
-    public static void printTriangle(int number) {
-        for(int i = 1; i <= number; ++i) {
-            printSpaces(number - i);
-            printStars(i);
-            System.out.println("");
-        }
-
-        System.out.println("");
-    }
-
     public static void printChristmasTree(int number) {
+        // final method that make the body of christmas tree
         int cntr = 1;
         --number;
 
@@ -45,7 +38,7 @@ public class Main {
             cntr += 2;
             System.out.println("");
         }
-
+        //for printing base of tree
         printSpaces(number - 2);
         printStars(3);
 
